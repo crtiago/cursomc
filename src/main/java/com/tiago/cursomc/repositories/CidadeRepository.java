80abe75b94a90b10b6832b83c5d6033a54cb91aa
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2019-04-16 16:01:02 -0300

    Estado e cidade

---
+++ src/main/java/com/tiago/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,17 @@
+package com.tiago.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tiago.cursomc.domain.Cidade;
+
+/*
+ * Classe capaz de acessar o BD,fazer as consultas para acessar os dados da tabela Categoria
+ * JpaRepository tipo especial do Spring capaz de acessar os dados com base em um tipo que você 
+ * passar que no caso é Categoria,e também deve colocar qual o tipo do atributo identificador desse obejto
+ * que no caso é Integer
+ */
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
